b1a0fa72c057fd1ec4d427f5b9d6cbdabe1fce72
Tuba Areen <tubaareen00@gmail.com>
2025-11-11 15:00:00 +0530

    sg-variables

---
+++ modules/sg/variable.tf
@@ -0,0 +1,31 @@
+variable "name" {
+    type = string
+}
+
+variable "vpc_id" {
+    type = string
+}
+
+variable "onprem_mysql_password" {
+    type      = string
+    sensitive = true
+}
+
+variable "dest_mysql_password" {
+    type      = string
+    sensitive = true
+    default   = "admin123!"
+}
+
+variable "ports" {
+    type = map(string)
+    default = {
+        3306 = "10.0.0.0/16"  # FIXED: Valid CIDR
+    }
+}
+
+variable "admin_cidr" {
+  type        = string
+  description = "Admin IP CIDR for SSH"
+  default     = "0.0.0.0/0"  # Override in production
+}
