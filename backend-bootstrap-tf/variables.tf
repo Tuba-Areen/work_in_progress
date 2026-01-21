32c1596e100668105669eef5bcb1030fc1f4700c
Tuba Areen <tubaareen00@gmail.com>
2026-01-21 16:33:52 +0530

    ..

---
+++ backend-bootstrap-tf/variables.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project" {
+  description = "Project name"
+  type        = string
+}
+
+variable "environment" {
+  description = "Environment name"
+  type        = string
+}
