35d6d2ec31bfd0b046779601eecdae9a83c03d6e
Tuba Areen <tubaareen00@gmail.com>
2026-01-24 12:00:00 +0530

    .

--- main.tf
+++ main.tf
@@ -102,13 +102,3 @@ module "dms" {
   depends_on = [module.onprem_mysql, module.rds_target]
 }
 
-
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-<project>-<env>-<account-id>"
-    key            = "dms/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "terraform-lock-<project>-<env>"
-    encrypt        = true
-  }
-}
